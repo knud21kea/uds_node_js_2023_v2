c9f80ac74e94cfd51aeaa2f55bcf03bd1763806b
knud21kea <grah0070@stud.kea.dk>
2023-08-23 11:44:11 +0200

    objects and arrays solved

--- 00._Miscellaneous/01._Intro_Exercises/03.Objects.js
+++ 00._Miscellaneous/01._Intro_Exercises/03.Objects.js
@@ -5,13 +5,18 @@
 
 const myObj = { message: "Hello, earthling! I bring peace." };
 
-// Log the message 
+// Log the message
+
+const messageToLog = myObj.message;
+console.log(messageToLog);
 
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const me = { name: "Graham", age: 61}; // ancient
+console.log(me);
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -20,6 +25,8 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,5 +34,7 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute";
 
 // --------------------------------------
--- 00._Miscellaneous/01._Intro_Exercises/04.Arrays.js
+++ 00._Miscellaneous/01._Intro_Exercises/04.Arrays.js
@@ -6,6 +6,8 @@
 const letters = ["a","b","c"];
 // show b in the console 
 
+const elementToShow = letters[1];
+console.log(elementToShow);
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -14,6 +16,10 @@ const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
 
+const friend1 = { name: "Ali"};
+const friend2 = { name: "Bob"};
+const friend3 = { name: "Cal"};
+friends.push('friend1', 'friend2', 'friend3');
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
@@ -21,8 +27,10 @@ const friends = [];
 const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 
 // You want to programmatically find where the number 1729 is in the array.
-// programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
+// programmatically means that no finger counting allowed. There is a method for this (finding index based of value).
 
+const index = significantMathNumbers.indexOf(1729); // Still returns first index if there are multiple entries
+console.log(index);
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -31,8 +39,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
-
-
+diet.splice( 2, 0, "hamburger", "soda", "pizza"); // 0 elements deleted
+console.log(diet);
 
 // --------------------------------------
 // Exercise 5 - Remove element
@@ -40,16 +48,16 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
-
-
-
+diet.pop();
+console.log(diet);
 
 // --------------------------------------
 // Exercise 6 - Copy array
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
+const dinnerTray = [...diet]; // could use slice instead of spread
+console.log("new array is " , dinnerTray);
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -58,7 +66,9 @@ const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
 
-
+for (i = 1; i < lettersExpanded.length; i = i + 2) {
+    console.log(lettersExpanded[i]);
+}
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
@@ -69,5 +79,14 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
+
+numbers.forEach(number => {
+    if (number > 6 || number < 0) {
+        console.log(number);
+    } else {
+        discardedNumbers.push(number);
+    }
+});
+console.log(discardedNumbers);
 
 // --------------------------------------
